5fe9b51c6861114098407e49ac0e875d8ba95454
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-12-05 18:20:26 +0400

    Add-ons installation and samples

---
+++ content/modules/rest/examples/ex1/src/main/java/sample/SampleRestApplication.java
@@ -0,0 +1,31 @@
+package sample;
+
+import io.jmix.core.security.CoreSecurityConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class SampleRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleRestApplication.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "main.datasource")
+    DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @EnableWebSecurity
+    static class SecurityConfiguration extends CoreSecurityConfiguration {
+    }
+}
