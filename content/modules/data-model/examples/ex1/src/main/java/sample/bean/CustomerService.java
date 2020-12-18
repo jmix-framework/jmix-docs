2d19efd4743a4dd21598ae6234904950a86879fa
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-12-18 14:45:23 +0400

    Metadata

--- content/modules/data-model/examples/ex1/src/main/java/sample/bean/CustomerService.java
+++ content/modules/data-model/examples/ex1/src/main/java/sample/bean/CustomerService.java
@@ -2,12 +2,19 @@ package sample.bean;
 
 import io.jmix.core.DataManager;
 import io.jmix.core.Id;
+import io.jmix.core.Metadata;
 import io.jmix.core.SaveContext;
+import io.jmix.core.metamodel.datatype.Datatype;
+import io.jmix.core.metamodel.datatype.Enumeration;
+import io.jmix.core.metamodel.model.MetaClass;
+import io.jmix.core.metamodel.model.MetaProperty;
+import io.jmix.core.metamodel.model.Range;
 import io.jmix.data.PersistenceHints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sample.entity.Customer;
+import sample.entity.Order;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -47,4 +54,42 @@ public class CustomerService {
         entityManager.remove(customer);
     }
     // end::hard-delete-em[]
+
+    // tag::metadata[]
+    @Autowired
+    private Metadata metadata;
+
+    public void printOrderProperties() {
+        MetaClass metaClass = metadata.getClass(Order.class); // <1>
+        for (MetaProperty metaProperty : metaClass.getProperties()) { // <2>
+
+            String propertyName = metaProperty.getName(); // <3>
+            MetaProperty.Type propertyType = metaProperty.getType(); // <4>
+            Class<?> javaType = metaProperty.getJavaType(); // <5>
+            Range propertyRange = metaProperty.getRange(); // <6>
+
+            String info = "name: " + propertyName +
+                    "\n type: " + propertyType +
+                    "\n Java type: " + javaType +
+                    "\n range: " + propertyRange;
+
+            if (propertyRange.isClass()) { // <7>
+                MetaClass refMetaClass = propertyRange.asClass(); // <8>
+                Range.Cardinality cardinality = propertyRange.getCardinality(); // <9>
+                info += "\n reference to: " + refMetaClass;
+                info += "\n cardinality: " + cardinality;
+
+            } else if (propertyRange.isEnum()) { // <10>
+                Enumeration<?> enumeration = propertyRange.asEnumeration(); // <11>
+                info += "\n enum: " + enumeration;
+
+            } else if (propertyRange.isDatatype()) { // <12>
+                Datatype<Object> propertyDatatype = propertyRange.asDatatype(); // <13>
+                info += "\n data type: " + propertyDatatype;
+            }
+
+            System.out.println(info);
+        }
+    }
+    // end::metadata[]
 }
