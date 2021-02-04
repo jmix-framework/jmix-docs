7f1195c13232066fe9af1b8e9b20527287e588ef
Mariya Sukhova <sukhova@haulmont.com>
2021-02-04 17:15:42 +0400

    Rename UI components #12 SuggestionField, iteration 1

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/suggestionfield/SuggestionFieldScreen.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.component.suggestionfield;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("suggestionField-screen")
+@UiDescriptor("suggestionfield-screen.xml")
+public class SuggestionFieldScreen extends Screen {
+}
