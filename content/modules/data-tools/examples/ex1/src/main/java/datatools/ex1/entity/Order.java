b7af30bc88578d23d008f3d34402d18d666b7a7b
zbitskaya <zbitskaya@haulmont.com>
2021-02-09 14:07:58 +0400

    Document Entity Inspector

---
+++ content/modules/data-tools/examples/ex1/src/main/java/datatools/ex1/entity/Order.java
@@ -0,0 +1,62 @@
+package datatools.ex1.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "SAMPLE_ORDER")
+@Entity(name = "sample_Order")
+public class Order {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "DATE_")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @InstanceName
+    @Column(name = "PRODUCT")
+    private String product;
+
+    @Column(name = "AMOUNT")
+    private Integer amount;
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
