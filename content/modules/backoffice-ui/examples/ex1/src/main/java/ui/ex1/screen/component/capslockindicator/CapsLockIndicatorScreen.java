4694a88c845f75771c3207c1b187063075d5b2e4
Georgii Vlasov <g.vlasov@haulmont.com>
2021-02-26 12:32:50 +0400

    Add Backoffice-ui CapsLockIndicator #45

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/capslockindicator/CapsLockIndicatorScreen.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.component.capslockindicator;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("CapsLockIndicatorScreen")
+@UiDescriptor("caps-lock-indicator-screen.xml")
+public class CapsLockIndicatorScreen extends Screen {
+}
