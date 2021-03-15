3a1c62cb9adfecc251f9d6c5155304c7be3f9578
zbitskaya <zbitskaya@haulmont.com>
2021-03-15 12:21:00 +0400

    BPM: Intro section #104

---
+++ content/modules/bpm/examples/ex1/src/main/java/bpm/ex1/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package bpm.ex1.screen.user;
+
+import bpm.ex1.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("smpl_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
