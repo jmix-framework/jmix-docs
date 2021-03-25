d0c349420a433f49d62ffaa91188ffa74632f386
Georgii Vlasov <g.vlasov@haulmont.com>
2021-03-25 20:00:52 +0400

    Refactoring CheckBoxGroup in Backoffice-ui #33, iteration 1

--- content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/checkboxgroup/CheckBoxGroupScreen.java
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/checkboxgroup/CheckBoxGroupScreen.java
@@ -4,19 +4,16 @@ import io.jmix.ui.component.CheckBoxGroup;
 import io.jmix.ui.component.data.options.ContainerOptions;
 import io.jmix.ui.component.data.options.EnumOptions;
 import io.jmix.ui.model.CollectionContainer;
-import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import ui.ex1.entity.Country;
-import ui.ex1.entity.Customer;
-import ui.ex1.entity.Level;
+import ui.ex1.entity.*;
 
 @UiController("CheckBoxGroupScreen")
 @UiDescriptor("check-box-group-screen.xml")
 public class CheckBoxGroupScreen extends Screen {
     // tag::simple-check-box-1[]
     @Autowired
-    private CheckBoxGroup<Level> checkBoxGroup;
+    private CheckBoxGroup<Operation> checkBoxGroup;
 
     // end::simple-check-box-1[]
 
@@ -25,19 +22,12 @@ public class CheckBoxGroupScreen extends Screen {
     private CheckBoxGroup<Country> countriesCheckBoxGroup;
     @Autowired
     private CollectionContainer<Country> countriesDc;
+
     // end::countries-dc-1[]
-
-    // tag::countries-dl-1[]
-    @Autowired
-    private CollectionLoader<Country> countriesDl;
-
-    // end::countries-dl-1[]
 
     // tag::customers-description-1[]
     @Autowired
     private CheckBoxGroup<Customer> checkBoxGroupCustomer;
-    @Autowired
-    private CollectionLoader<Customer> customersDl;
 
     // end::customers-description-1[]
 
@@ -46,25 +36,17 @@ public class CheckBoxGroupScreen extends Screen {
     protected void onInit(InitEvent event) {
         // end::start-init[]
 
-        // tag::enum-check-box-2[]
-        checkBoxGroup.setOptionsEnum(Level.class);
-        // end::enum-check-box-2[]
-
         // tag::options-check-box-2[]
-        checkBoxGroup.setOptions(new EnumOptions<>(Level.class));
+        checkBoxGroup.setOptions(new EnumOptions<>(Operation.class));
         // end::options-check-box-2[]
 
         // tag::countries-dc-2[]
         countriesCheckBoxGroup.setOptions(new ContainerOptions<>(countriesDc));
-        // tag::countries-dl-2[]
-        countriesDl.load();
-        // end::countries-dl-2[]
         // end::countries-dc-2[]
 
         // tag::customers-description-2[]
         checkBoxGroupCustomer.setOptionDescriptionProvider(customer ->
                 "Email: " + customer.getEmail());
-        customersDl.load();
         // end::customers-description-2[]
 
         // tag::end-init[]
@@ -76,11 +58,11 @@ public class CheckBoxGroupScreen extends Screen {
     private String checkBoxGroupOptionDescriptionProvider(Level level) {
         switch (level) {
             case PLATINUM:
-                return "Senior";
+                return "Platinum level";
             case GOLD:
-                return "Middle";
+                return "Gold level";
             default:
-                return "Junior";
+                return "Silver level";
         }
     }
     // end::install[]
