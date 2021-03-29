1a6d33b10a6585eb62547261d38ebe005f8e647c
bryanyin <hanbing.yin@icloud.com>
2021-03-29 11:42:00 +0800

    refine data structure

--- content/modules/reports/examples/ex1/src/main/java/com/company/jmixreports/screen/bookpublication/BookPublicationBrowse.java
+++ content/modules/reports/examples/ex1/src/main/java/com/company/jmixreports/screen/bookpublication/BookPublicationBrowse.java
@@ -1,10 +1,29 @@
 package com.company.jmixreports.screen.bookpublication;
 
+import io.jmix.reportsui.action.list.ExecutionHistoryAction;
+import io.jmix.ui.Actions;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.Button;
 import io.jmix.ui.screen.*;
 import com.company.jmixreports.entity.BookPublication;
+
+import javax.inject.Inject;
 
 @UiController("jmxrpr_BookPublication.browse")
 @UiDescriptor("book-publication-browse.xml")
 @LookupComponent("bookPublicationsTable")
 public class BookPublicationBrowse extends StandardLookup<BookPublication> {
+    // tag::report-history-button[]
+    @Inject
+    protected Actions actions;
+
+    @Inject
+    protected Button execHistoryBtn;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        ExecutionHistoryAction action = actions.create(ExecutionHistoryAction.class, "execHistoryReport");
+        execHistoryBtn.setAction(action);
+    }
+    // tag::report-history-button[]
 }
