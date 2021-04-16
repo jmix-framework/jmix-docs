72ef3e969fb88ad119625fc1fb8a1321b1d8c8ad
Georgii Vlasov <g.vlasov@haulmont.com>
2021-04-16 15:16:19 +0400

    Add DateField component in backoffice-ui #46

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/datefield/DateFieldScreen.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.component.datefield;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("DateFieldScreen")
+@UiDescriptor("date-field-screen.xml")
+public class DateFieldScreen extends Screen {
+}
