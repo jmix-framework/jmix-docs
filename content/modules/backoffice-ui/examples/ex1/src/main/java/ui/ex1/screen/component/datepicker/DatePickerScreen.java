4602657114785809fd4b096ee7f3bbaab7be4021
Georgii Vlasov <g.vlasov@haulmont.com>
2021-04-16 15:18:52 +0400

    Add DatePicker component in backoffice-ui #47

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/datepicker/DatePickerScreen.java
@@ -0,0 +1,12 @@
+package ui.ex1.screen.component.datepicker;
+
+import io.jmix.ui.component.DatePicker;
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("sample_DatePickerScreen")
+@UiDescriptor("date-picker-screen.xml")
+public class DatePickerScreen extends Screen {
+}
