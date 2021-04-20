efa420fb73a4b69d8401a12d05706ea32339a5d1
zbitskaya <zbitskaya@haulmont.com>
2021-04-20 13:47:32 +0400

    BPM: Service Task #117

---
+++ content/modules/bpm/examples/ex1/src/main/java/bpm/ex1/service/SendEmailJavaDelegate.java
@@ -0,0 +1,41 @@
+package bpm.ex1.service;
+
+import bpm.ex1.entity.User;
+import io.jmix.email.EmailInfo;
+import io.jmix.email.EmailInfoBuilder;
+import io.jmix.email.Emailer;
+import jdk.internal.joptsimple.internal.Strings;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.flowable.common.engine.api.delegate.Expression;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// tag::java-delegate[]
+@Component("smpl_SendEmailJavaDelegate")
+public class SendEmailJavaDelegate implements JavaDelegate {
+
+    private Expression addressee; // <1>
+    private Expression emailSubject;
+    private Expression emailBody;
+
+    @Autowired
+    private Emailer emailer;
+
+    @Override
+    public void execute(DelegateExecution execution) { // <2>
+        User addresseeValue = (User) addressee.getValue(execution); // <3>
+        String emailSubjectValue = (String) emailSubject.getValue(execution);
+        String emailBodyValue = (String) emailBody.getValue(execution);
+        EmailInfo emailInfo = EmailInfoBuilder.create() // <4>
+                .setAddresses(addresseeValue.getEmail())
+                .setSubject(emailSubjectValue)
+                .setFrom(null)
+                .setBody(emailBodyValue)
+                .build();
+        emailer.sendEmailAsync(emailInfo); // <5>
+    }
+}
+// end::java-delegate[]
