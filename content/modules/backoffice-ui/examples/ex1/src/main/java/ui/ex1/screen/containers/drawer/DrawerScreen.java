a4c21fb1db620f93daf425ed26260fcb7c73f37a
Georgii Vlasov <g.vlasov@haulmont.com>
2021-05-13 18:27:01 +0400

    Add Drawer #140 in Backoffice-ui

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/containers/drawer/DrawerScreen.java
@@ -0,0 +1,31 @@
+package ui.ex1.screen.containers.drawer;
+
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.mainwindow.Drawer;
+import io.jmix.ui.icon.JmixIcon;
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("sample_DrawerScreen")
+@UiDescriptor("drawer-screen.xml")
+public class DrawerScreen extends Screen {
+    // tag::drawer[]
+    @Autowired
+    private Drawer drawer;
+    @Autowired
+    private Button collapseDrawerButton;
+
+    @Subscribe("collapseDrawerButton")
+    public void onCollapseDrawerButtonClick(Button.ClickEvent event) {
+        drawer.toggle();
+        if (drawer.isCollapsed()) {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_RIGHT);
+        } else {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_LEFT);
+        }
+    }
+    // end::drawer[]
+}
