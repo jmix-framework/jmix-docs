29fd60908b9456ff40cadab00c3ff10175f1deeb
Mariya Sukhova <sukhova@haulmont.com>
2021-05-14 15:32:08 +0400

    Document the Filter component #42 iteration 1

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/filter/FilterScreen.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.component.filter;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("filterScreen")
+@UiDescriptor("filter-screen.xml")
+public class FilterScreen extends Screen {
+}
