5b3718e75f7f25f3822c37fd8b651d00060c4189
Mariya Sukhova <sukhova@haulmont.com>
2021-05-17 14:36:00 +0400

    Document PropertyFilter UI component #1 Changes made in accordance with the review

--- content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/propertyfilter/PropertyFilterScreen.java
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/propertyfilter/PropertyFilterScreen.java
@@ -1,27 +1,34 @@
 package ui.ex1.screen.component.propertyfilter;
 
-import io.jmix.core.DataManager;
-import io.jmix.core.querycondition.PropertyCondition;
-import io.jmix.ui.component.Button;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.Subscribe;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.PropertyFilter;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import ui.ex1.entity.Customer;
 
 @UiController("propertyFilterScreen")
 @UiDescriptor("property-filter-screen.xml")
 public class PropertyFilterScreen extends Screen {
     @Autowired
-    private DataManager dataManager;
+    private Notifications notifications;
 
-    @Subscribe("btn")
-    public void onBtnClick(Button.ClickEvent event) {
-        // tag::property-condition[]
-        dataManager.load(Customer.class)
-                .condition(PropertyCondition.contains("firstName", "a"))
-                .list();
-        // end::property-condition[]
+    // tag::operation-change-event[]
+    @Subscribe("hobbyFilter")
+    public void onHobbyFilterOperationChange(PropertyFilter.OperationChangeEvent event) {
+        notifications.create()
+                .withCaption("Before: " + event.getPreviousOperation() +
+                        ". After: " + event.getNewOperation())
+                .show();
     }
+    // end::operation-change-event[]
+
+    // tag::value-change-event[]
+    @Subscribe("hobbyFilter")
+    public void onHobbyFilterValueChange(HasValue.ValueChangeEvent event) {
+        notifications.create()
+                .withCaption("Before: " + event.getPrevValue() +
+                        ". After: " + event.getValue())
+                .show();
+    }
+    // end::value-change-event[]
 }
