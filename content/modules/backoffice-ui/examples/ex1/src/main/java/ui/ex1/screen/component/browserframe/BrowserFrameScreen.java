51b6fe1b5730da3425930b9aa4ce044e3f075ec4
Georgii Vlasov <honest.niceman@gmail.com>
2021-05-30 00:55:38 +0400

    Add BrowserFrame #43 in Backoffice-ui

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/browserframe/BrowserFrameScreen.java
@@ -0,0 +1,8 @@
+package ui.ex1.screen.component.browserframe;
+
+import io.jmix.ui.screen.*;
+
+@UiController("sample_BrowserFrameScreen")
+@UiDescriptor("browser-frame-screen.xml")
+public class BrowserFrameScreen extends Screen {
+}
