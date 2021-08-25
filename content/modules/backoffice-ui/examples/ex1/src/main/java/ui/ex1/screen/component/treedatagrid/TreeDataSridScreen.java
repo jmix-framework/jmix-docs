6a9c9b0b4f5f9a796c3081068073cb58f94f5de2
Mariya Sukhova <sukhova@haulmont.com>
2021-08-25 19:28:51 +0400

    Document the TreeDataGrid component #37

---
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/component/treedatagrid/TreeDataSridScreen.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.component.treedatagrid;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("treeDataGrid-screen")
+@UiDescriptor("treeDataGrid-screen.xml")
+public class TreeDataSridScreen extends Screen {
+}
