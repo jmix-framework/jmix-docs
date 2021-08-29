b1b86c8d9c70c8f90db6a9c0afd981fbfaa92e71
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-08-29 18:38:45 +0400

    Deploy on Kubernetes #253

---
+++ content/modules/deployment/examples/ex1/src/main/java/deployment/ex1/SampleApplication.java
@@ -0,0 +1,25 @@
+package deployment.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix="main.datasource")
+	DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+}
---
+++ content/modules/deployment/examples/ex1/src/main/java/deployment/ex1/screen/customer/CustomerEdit.java
@@ -0,0 +1,13 @@
+package deployment.ex1.screen.customer;
+
+import deployment.ex1.entity.Customer;
+import io.jmix.ui.screen.EditedEntityContainer;
+import io.jmix.ui.screen.StandardEditor;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("sample_Customer.edit")
+@UiDescriptor("customer-edit.xml")
+@EditedEntityContainer("customerDc")
+public class CustomerEdit extends StandardEditor<Customer> {
+}
