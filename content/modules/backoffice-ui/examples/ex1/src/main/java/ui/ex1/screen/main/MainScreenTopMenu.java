d84518bb9f9117946ec14f2ba8fa7918f3a9de0d
Mariya Sukhova <sukhova@haulmont.com>
2021-08-31 16:04:36 +0400

    Document the AppMenu component #60

--- content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/main/MainScreenTopMenu.java
+++ content/modules/backoffice-ui/examples/ex1/src/main/java/ui/ex1/screen/main/MainScreenTopMenu.java
@@ -1,8 +1,10 @@
 package ui.ex1.screen.main;
 
+import io.jmix.ui.Notifications;
 import io.jmix.ui.ScreenTools;
 import io.jmix.ui.component.AppWorkArea;
 import io.jmix.ui.component.Window;
+import io.jmix.ui.component.mainwindow.AppMenu;
 import io.jmix.ui.component.mainwindow.UserIndicator;
 import io.jmix.ui.navigation.Route;
 import io.jmix.ui.screen.Screen;
@@ -12,10 +14,12 @@ import io.jmix.ui.screen.UiControllerUtils;
 import io.jmix.ui.screen.UiDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+// tag::main-top-start[]
 @UiController("MainTop")
 @UiDescriptor("main-screen-top-menu.xml")
 @Route(path = "main", root = true)
 public class MainScreenTopMenu extends Screen implements Window.HasWorkArea {
+    // end::main-top-start[]
 
     // tag::user-indicator[]
     @Autowired
@@ -28,6 +32,13 @@ public class MainScreenTopMenu extends Screen implements Window.HasWorkArea {
 
     @Autowired
     private AppWorkArea workArea;
+    // tag::inject-for-menu[]
+    @Autowired
+    private AppMenu mainMenu;
+    @Autowired
+    private Notifications notifications;
+
+    // end::inject-for-menu[]
 
     @Override
     public AppWorkArea getWorkArea() {
@@ -48,4 +59,39 @@ public class MainScreenTopMenu extends Screen implements Window.HasWorkArea {
         userIndicator.setFormatter(value -> value.getUsername() + " - user");
     }
 // end::formatter[]
+
+    // tag::on-init-for-menu[]
+    @Subscribe
+    public void onInit(InitEvent event) {
+        AppMenu.MenuItem rootItem = mainMenu.createMenuItem("help", "Help");
+        AppMenu.MenuItem subItemStarted = mainMenu.createMenuItem("start",
+                "Getting Started");
+        AppMenu.MenuItem subItemNews = mainMenu.createMenuItem("news", "News",
+                "font-icon:NEWSPAPER_O", null); // <1>
+        AppMenu.MenuItem subItemBlog = mainMenu.createMenuItem("blog", "Blog");
+        AppMenu.MenuItem subItemSite = mainMenu.createMenuItem("site", "Website");
+        AppMenu.MenuItem subItemAbout = mainMenu.createMenuItem("about", "About",
+                null, menuItem -> { // <2>
+            notifications.create()
+                    .withCaption("About menu item clicked")
+                    .withType(Notifications.NotificationType.HUMANIZED)
+                    .show();
+        });
+        AppMenu.MenuItem subItemCenter = mainMenu.createMenuItem("center",
+                "Client Center");
+
+        subItemNews.addChildItem(subItemBlog);
+        subItemNews.addChildItem(subItemSite);
+
+        rootItem.addChildItem(subItemStarted);
+        rootItem.addChildItem(subItemNews);
+        rootItem.addChildItem(subItemAbout);
+        rootItem.addChildItem(mainMenu.createSeparator()); // <3>
+        rootItem.addChildItem(subItemCenter);
+
+        mainMenu.addMenuItem(rootItem, 0);
+    }
+    // end::on-init-for-menu[]
+    // tag::main-top-end[]
 }
+// end::main-top-end[]
