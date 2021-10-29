f5bf929d011d7dfd932907524ea5103ae7c396c5
Mariya Sukhova <sukhova@haulmont.com>
2021-10-29 18:58:17 +0400

    Document ui-export addon functionality #160

---
+++ content/modules/grid-export/examples/ex1/src/main/java/gridexport/ex1/screen/exporter/CustomExporter.java
@@ -0,0 +1,51 @@
+package gridexport.ex1.screen.exporter;
+
+import io.jmix.ui.component.DataGrid;
+import io.jmix.ui.component.Table;
+import io.jmix.ui.download.Downloader;
+import io.jmix.uiexport.exporter.ExportMode;
+import io.jmix.uiexport.exporter.TableExporter;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+
+// tag::my-custom-exporter[]
+@Component("ui_CustomExporter")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CustomExporter implements TableExporter {
+    @Override
+    public void exportTable(Downloader downloader, Table<Object> table, ExportMode exportMode) {
+        //your custom logic
+    }
+
+    @Override
+    public void exportDataGrid(Downloader downloader, DataGrid<Object> dataGrid, ExportMode exportMode) {
+        //your custom logic
+    }
+
+    @Override
+    public String getCaption() {
+        return "custom";
+    }
+
+    @Override
+    public void addColumnValueProvider(String columnId, Function<ColumnValueContext, Object> columnValueProvider) {
+        //your custom logic
+    }
+
+    @Override
+    public void removeColumnValueProvider(String columnId) {
+        //your custom logic
+    }
+
+    @Nullable
+    @Override
+    public Function<ColumnValueContext, Object> getColumnValueProvider(String columnId) {
+        //your custom logic
+        return null;
+    }
+}
+// end::my-custom-exporter[]
