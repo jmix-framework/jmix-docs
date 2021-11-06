d37114aa4b60c1aae0e641dfbc43adcc765d8926
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-11-06 14:10:34 +0400

    Add to Row-level Roles about applicationContext variable in Groovy predicates

--- content/modules/security/examples/ex1/src/main/java/security/ex1/security/restrictions/TestRowLevelPredicateRole.java
+++ content/modules/security/examples/ex1/src/main/java/security/ex1/security/restrictions/TestRowLevelPredicateRole.java
@@ -2,12 +2,11 @@ package security.ex1.security.restrictions;
 
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.security.model.RowLevelPolicyAction;
+import io.jmix.security.model.RowLevelPredicate;
 import io.jmix.security.role.annotation.PredicateRowLevelPolicy;
 import io.jmix.security.role.annotation.RowLevelRole;
 import security.ex1.entity.Customer;
 import security.ex1.entity.User;
-
-import java.util.function.Predicate;
 
 @RowLevelRole(
         name = "Can see Orders with amount < 1000, Can see Customers of their region",
@@ -17,7 +16,7 @@ public interface TestRowLevelPredicateRole {
     @PredicateRowLevelPolicy(
             entityClass = Customer.class,
             actions = {RowLevelPolicyAction.READ})
-    default Predicate<Customer> customerOfMyRegion(CurrentAuthentication currentAuthentication) {
+    default RowLevelPredicate<Customer> customerOfMyRegion(CurrentAuthentication currentAuthentication) {
         return customer -> customer.getRegion() != null
                 && customer.getRegion().equals(((User) currentAuthentication.getUser()).getRegion());
     }
