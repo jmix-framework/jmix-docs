209fe6d1b9ffc9a3c636b6b3fb31806fdd31593e
Mariya Sukhova <sukhova@haulmont.com>
2022-02-14 14:54:20 +0400

    Business Calendar add-on #331

---
+++ content/modules/business-calendar/examples/ex1/src/main/java/businesscalendar/ex1/calendars/SampleBusinessCalendar.java
@@ -0,0 +1,45 @@
+package businesscalendar.ex1.calendars;
+
+import io.jmix.businesscalendar.annotation.*;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+
+// tag::business-calendar[]
+@BusinessCalendar(name = "Sample Business Calendar",
+                  code = "sample-business-calendar") // <1>
+public interface SampleBusinessCalendar {
+
+    @CronHoliday(expression = "* * * 1-2 MAY ?") // <2>
+    @CronHoliday(expression = "* * * ? * 6#3")
+    void cronHoliday();
+
+    @WeeklyHoliday(DayOfWeek.SATURDAY)
+    @WeeklyHoliday(value = DayOfWeek.SUNDAY,
+            description = "Sunday is always a day for family") // <3>
+    void weeklyHolidays();
+
+    @FixedDayHoliday(fixedDate = "2021-05-08") // <4>
+    @FixedDayHoliday(fixedDate = "2021-05-09")
+    void fixedHoliday();
+
+    @FixedYearlyHoliday(month = Month.NOVEMBER, dayOfMonth = 4) // <5>
+    @FixedYearlyHoliday(month = Month.JUNE, dayOfMonth = 12)
+    void fixedYearlyHoliday();
+
+    @ScheduledBusinessDay(dayOfWeek = DayOfWeek.MONDAY,
+            startTime = "08:00", endTime = "17:00") // <6>
+    @ScheduledBusinessDay(dayOfWeek = DayOfWeek.WEDNESDAY,
+            startTime = "09:00", endTime = "17:00")
+    @ScheduledBusinessDay(dayOfWeek = DayOfWeek.FRIDAY,
+            startTime = "10:00", endTime = "15:00")
+    void scheduledBD();
+
+    @AdditionalBusinessDay(fixedDate = "2021-05-06",
+            startTime = "10:00", endTime = "16:30") // <7>
+    @AdditionalBusinessDay(fixedDate = "2021-05-07",
+            startTime = "10:00", endTime = "16:30")
+    void additionalBD();
+
+}
+// end::business-calendar[]
