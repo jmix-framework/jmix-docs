c96702882e7c19d6fa734294aaf7f7252c0af924
oshiryaeva <gospellcheck@gmail.com>
2022-03-22 15:31:20 +0400

    Search: Programmatic mapping update #243

--- content/modules/search/examples/ex1/src/main/java/search/ex1/service/CustomerIndexDefinition.java
+++ content/modules/search/examples/ex1/src/main/java/search/ex1/service/CustomerIndexDefinition.java
@@ -2,8 +2,10 @@ package search.ex1.service;
 
 import io.jmix.search.index.annotation.AutoMappedField;
 import io.jmix.search.index.annotation.JmixEntitySearchIndex;
+import io.jmix.search.index.annotation.ManualMappingDefinition;
 import io.jmix.search.index.mapping.MappingDefinition;
 import io.jmix.search.index.mapping.MappingDefinitionElement;
+import io.jmix.search.index.mapping.propertyvalue.impl.FilePropertyValueExtractor;
 import io.jmix.search.index.mapping.strategy.impl.AutoMappingStrategy;
 import search.ex1.entity.Customer;
 
@@ -21,12 +23,15 @@ public interface CustomerIndexDefinition {
     // end::method[]
 
     // tag::manual-mapping[]
-    default MappingDefinition mapping() {
+    @ManualMappingDefinition // <1>
+    default MappingDefinition mapping(FilePropertyValueExtractor filePropertyValueExtractor) { // <2>
         return MappingDefinition.builder()
                 .addElement(
                         MappingDefinitionElement.builder()
-                                .includeProperties("*")
-                                .withFieldMappingStrategyClass(AutoMappingStrategy.class)
+                                .includeProperties("*") // <3>
+                                .excludeProperties("hobby", "maritalStatus") // <4>
+                                .withFieldMappingStrategyClass(AutoMappingStrategy.class) // <5>
+                                .withPropertyValueExtractor(filePropertyValueExtractor) // <6>
                                 .build()
                 )
                 .build();
