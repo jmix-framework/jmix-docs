54791533d9d9f646e66fa7431ebf3e784c65f138
Mariya Sukhova <sukhova@haulmont.com>
2022-04-14 14:59:31 +0400

    Document custom WidgetSets creation #270 Changes were made according to the JD-CR-64 review

--- content/modules/ui/examples/ex1/src/main/java/ui/ex1/widgets/client/ColorButtonConnector.java
+++ content/modules/ui/examples/ex1/src/main/java/ui/ex1/widgets/client/ColorButtonConnector.java
@@ -7,19 +7,19 @@ import io.jmix.ui.widget.client.button.JmixButtonConnector;
 import ui.ex1.widgets.ColorButton;
 
 // tag::color-button-connector[]
-@Connect(ColorButton.class)
+@Connect(ColorButton.class) // <1>
 public class ColorButtonConnector extends JmixButtonConnector {
 
     @Override
     public ColorButtonState getState() {
-        return (ColorButtonState) super.getState(); // <1>
+        return (ColorButtonState) super.getState(); // <2>
     }
 
     @Override
-    public void onStateChanged(StateChangeEvent event) { // <2>
+    public void onStateChanged(StateChangeEvent event) { // <3>
         super.onStateChanged(event);
 
-        if (event.hasPropertyChanged("color")) { // <3>
+        if (event.hasPropertyChanged("color")) { // <4>
             Style style = getWidget().getElement().getStyle();
             style.setBackgroundColor(getState().color);
             style.setBackgroundImage("none");
