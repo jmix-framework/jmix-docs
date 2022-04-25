55389450844b647b67007eac2b9120335a5ef85b
Olga Shiryaeva <o.shiryaeva@haulmont.com>
2022-04-25 14:23:50 +0400

    Document default conversion error message for custom datatypes #267

--- content/modules/data-model/examples/ex1/src/main/java/datamodel/ex1/screen/model/ModelEdit.java
+++ content/modules/data-model/examples/ex1/src/main/java/datamodel/ex1/screen/model/ModelEdit.java
@@ -1,10 +1,24 @@
 package datamodel.ex1.screen.model;
 
+import io.jmix.ui.component.TextField;
 import io.jmix.ui.screen.*;
 import datamodel.ex1.entity.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("sample_Model.edit")
 @UiDescriptor("model-edit.xml")
 @EditedEntityContainer("modelDc")
 public class ModelEdit extends StandardEditor<Model> {
+
+    // tag::conversion-error[]
+    @Autowired
+    private TextField<Integer> productionYearField;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        productionYearField.setConversionErrorMessage("Incorrect year format");
+    }
+    // end::conversion-error[]
+
+
 }
