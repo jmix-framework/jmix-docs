fa50a034d18e3de21103ed9791328bc7c413dd38
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-04-29 10:19:26 +0400

    JPQL Extensions #185

--- content/modules/security/examples/ex1/src/main/java/security/ex1/screen/customer/CustomerBrowse.java
+++ content/modules/security/examples/ex1/src/main/java/security/ex1/screen/customer/CustomerBrowse.java
@@ -1,5 +1,6 @@
 package security.ex1.screen.customer;
 
+import io.jmix.core.session.SessionData;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,12 @@ import security.ex1.service.NotificationService;
 @UiController("sample_Customer.browse")
 @UiDescriptor("customer-browse.xml")
 @LookupComponent("customersTable")
+// tag::session-data[]
 public class CustomerBrowse extends StandardLookup<Customer> {
+
+    @Autowired
+    private SessionData sessionData;
+    // end::session-data[]
 
     @Autowired
     private NotificationService notificationService;
