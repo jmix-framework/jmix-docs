10545f312e60de80adeef29b08086ea5525dd072
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-05-10 19:00:48 +0400

    Fix description of default conversion error message for custom datatypes #267

--- content/modules/data-model/examples/ex1/src/main/java/datamodel/ex1/screen/model/ModelEdit.java
+++ content/modules/data-model/examples/ex1/src/main/java/datamodel/ex1/screen/model/ModelEdit.java
@@ -1,24 +1,13 @@
 package datamodel.ex1.screen.model;
 
-import io.jmix.ui.component.TextField;
-import io.jmix.ui.screen.*;
 import datamodel.ex1.entity.Model;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.jmix.ui.screen.EditedEntityContainer;
+import io.jmix.ui.screen.StandardEditor;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
 
 @UiController("sample_Model.edit")
 @UiDescriptor("model-edit.xml")
 @EditedEntityContainer("modelDc")
 public class ModelEdit extends StandardEditor<Model> {
-
-    // tag::conversion-error[]
-    @Autowired
-    private TextField<Integer> productionYearField;
-
-    @Subscribe
-    public void onInit(InitEvent event) {
-        productionYearField.setConversionErrorMessage("Incorrect year format");
-    }
-    // end::conversion-error[]
-
-
 }
