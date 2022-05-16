834d52c0b7455a050401701060a5c486f97eda5a
Mariya Sukhova <sukhova@haulmont.com>
2022-05-16 14:46:39 +0400

    Add examples to Image component resources #378

--- content/modules/ui/examples/ex1/src/main/java/ui/ex1/screen/component/image/ImageResourceScreen.java
+++ content/modules/ui/examples/ex1/src/main/java/ui/ex1/screen/component/image/ImageResourceScreen.java
@@ -1,8 +1,8 @@
 package ui.ex1.screen.component.image;
 
 import io.jmix.core.DataManager;
-import io.jmix.core.Messages;
 import io.jmix.ui.JmixApp;
+import io.jmix.ui.Notifications;
 import io.jmix.ui.component.*;
 import io.jmix.ui.screen.Screen;
 import io.jmix.ui.screen.Subscribe;
@@ -21,7 +21,6 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.UUID;
 
 @UiController("sample_ImageResourceScreen")
 @UiDescriptor("image-resource-screen.xml")
@@ -32,39 +31,66 @@ public class ImageResourceScreen extends Screen {
     private Image image;
     @Autowired
     private DataManager dataManager;
-    @Autowired
-    private Messages messages;
     @Inject
     protected JmixApp app;
+    @Autowired
+    private Notifications notifications;
 
-    @Subscribe
-    public void onInit(InitEvent event) {
+
+    @Subscribe("classpathBtn")
+    public void onClasspathBtnClick(Button.ClickEvent event) {
         //tag::image-classpath[]
         image.setSource(ClasspathResource.class)
                 .setPath("ui/ex1/screen/component/image/jmix-logo.png");
         //end::image-classpath[]
+    }
 
-        //tag::image-file-resource[]
+    @Subscribe("fileBtn")
+    public void onFileBtnClick(Button.ClickEvent event) {
+
         File file = new File("D:\\JMIX\\jmix-logo.png");
-        image.setSource(FileResource.class)
-                .setFile(file);
-        //end::image-file-resource[]
+        if (file.exists()) {
+            //tag::image-file-resource[]
+            image.setSource(FileResource.class)
+                    .setFile(file);
+            //end::image-file-resource[]
+        } else {
+            notifications.create()
+                    .withCaption("Upload the D:\\JMIX\\jmix-logo.png file")
+                    .show();
+        }
+    }
 
+    @Subscribe("fileStorageBtn")
+    public void onFileStorageBtnClick(Button.ClickEvent event) {
+        try {
+            Person person = dataManager.load(Person.class)
+                    .all().one();
+            if (person.getImage() != null)
+                //tag::image-file-storage-resource[]
+                image.setSource(FileStorageResource.class)
+                        .setFileReference(person.getImage());
+                //end::image-file-storage-resource[]
+            else notifications.create()
+                    .withCaption("Upload the image for the " + person.getName() + " person")
+                    .show();
+        } catch (IllegalStateException e) {
+            notifications.create()
+                    .withCaption("Create the person first!")
+                    .show();
+        }
+    }
 
-        UUID uuid = UUID.randomUUID();
-        Person person = dataManager.load(Person.class)
-                .id(uuid)
-                .one();
-        //tag::image-file-storage-resource[]
-        image.setSource(FileStorageResource.class)
-                .setFileReference(person.getImage());
-        //end::image-file-storage-resource[]
-
+    @Subscribe("relativePathBtn")
+    public void onRelativePathBtnClick(Button.ClickEvent event) {
         //tag::image-relative-path[]
         image.setSource(RelativePathResource.class)
-                .setPath("images/logo.png");
+                .setPath("images/jmix-icon-login.svg");
         //end::image-relative-path[]
+    }
 
+    @Subscribe("streamBtn")
+    public void onStreamBtnClick(Button.ClickEvent event) {
         try {
             URLConnection connection = new URL("https://picsum.photos/300").openConnection();
             InputStream inputStream = connection.getInputStream();
@@ -80,12 +106,18 @@ public class ImageResourceScreen extends Screen {
         } catch (IOException e) {
             throw new RuntimeException("Error getting image", e);
         }
+    }
 
+    @Subscribe("themeBtn")
+    public void onThemeBtnClick(Button.ClickEvent event) {
         //tag::image-theme[]
         image.setSource(ThemeResource.class)
                 .setPath("images/jmix-icon-login.svg");
         //end::image-theme[]
+    }
 
+    @Subscribe("urlBtn")
+    public void onUrlBtnClick(Button.ClickEvent event) {
         //tag::image-url[]
         try {
             image.setSource(UrlResource.class)
@@ -94,7 +126,6 @@ public class ImageResourceScreen extends Screen {
             log.error("Invalid URL format", e);
         }
         //end::image-url[]
-
     }
 
 
