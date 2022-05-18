b11923dafe6bc3331e12baa8482d772d3872b6c7
Mariya Sukhova <sukhova@haulmont.com>
2022-05-18 10:22:14 +0400

    Add tutorial on how to access a screen without login #384

---
+++ content/modules/ui/examples/ex2/src/main/java/ui/ex2/screen/mainscreensidemenu/MainScreenSideMenu.java
@@ -0,0 +1,50 @@
+package ui.ex2.screen.mainscreensidemenu;
+
+import io.jmix.ui.ScreenTools;
+import io.jmix.ui.component.AppWorkArea;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.Window;
+import io.jmix.ui.component.mainwindow.Drawer;
+import io.jmix.ui.icon.JmixIcon;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("MainScreenSideMenu")
+@UiDescriptor("main-screen-side-menu.xml")
+@Route(path = "main-anonymous", root = true)
+public class MainScreenSideMenu extends Screen implements Window.HasWorkArea {
+
+    @Autowired
+    private ScreenTools screenTools;
+
+    @Autowired
+    private AppWorkArea workArea;
+    @Autowired
+    private Drawer drawer;
+    @Autowired
+    private Button collapseDrawerButton;
+
+    @Override
+    public AppWorkArea getWorkArea() {
+        return workArea;
+    }
+
+    @Subscribe("collapseDrawerButton")
+    private void onCollapseDrawerButtonClick(Button.ClickEvent event) {
+        drawer.toggle();
+        if (drawer.isCollapsed()) {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_RIGHT);
+        } else {
+            collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_LEFT);
+        }
+    }
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+        screenTools.openDefaultScreen(
+                UiControllerUtils.getScreenContext(this).getScreens());
+
+        screenTools.handleRedirect();
+    }
+}
