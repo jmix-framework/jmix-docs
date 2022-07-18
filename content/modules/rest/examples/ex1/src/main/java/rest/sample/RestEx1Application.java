98ac241126d521cc5bbe3cd1d985b9e67047bd02
bryanyin <hanbing.yin@icloud.com>
2022-07-18 14:56:36 +0800

    sync to 1.3

--- content/modules/rest/examples/ex1/src/main/java/rest/sample/RestEx1Application.java
+++ content/modules/rest/examples/ex1/src/main/java/rest/sample/RestEx1Application.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -21,42 +24,61 @@ import java.util.Properties;
 @SpringBootApplication
 public class RestEx1Application {
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestEx1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestEx1Application.class, args);
+    }
 
-	@Bean
-	@Primary
-	BuildProperties buildProperties() {
-		Properties entries = new Properties();
-		entries.setProperty("version", "1.0");
-		entries.setProperty("name", "sample_rest");
-		return new BuildProperties(entries);
-	}
+    @Bean
+    @Primary
+    BuildProperties buildProperties() {
+        Properties entries = new Properties();
+        entries.setProperty("version", "1.0");
+        entries.setProperty("name", "sample_rest");
+        return new BuildProperties(entries);
+    }
 
-	@Bean
-	@Primary
-	@ConfigurationProperties("main.datasource")
-	DataSourceProperties dataSourceProperties() {
-		return new DataSourceProperties();
-	}
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource")
+    DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
 
 
-	@Bean
-	@Primary
-	@ConfigurationProperties("main.datasource.hikari")
-	DataSource dataSource(DataSourceProperties dataSourceProperties) {
-		return dataSourceProperties.initializeDataSourceBuilder().build();
-	}
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource.hikari")
+    DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
 
-	@EventListener
-	public void printApplicationUrl(ApplicationStartedEvent event) {
-		LoggerFactory.getLogger(RestEx1Application.class).info("Application started at "
-				+ "http://localhost:"
-				+ environment.getProperty("local.server.port")
-				+ Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
-	}
+    @EventListener
+    public void printApplicationUrl(ApplicationStartedEvent event) {
+        LoggerFactory.getLogger(RestEx1Application.class).info("Application started at "
+                + "http://localhost:"
+                + environment.getProperty("local.server.port")
+                + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
+    }
+
+    // tag::custom-cors[]
+    @Bean
+    public WebSecurityConfigurerAdapter mySecurityConfigurerAdapter() {
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http
+                        .requestMatchers(requestMatchers ->
+                                requestMatchers.antMatchers("/myapi/hello")
+                        )
+                        .cors(Customizer.withDefaults())
+                        .authorizeRequests(authorize ->
+                                authorize.anyRequest().permitAll()
+                        );
+            }
+        };
+    }
+    // end::custom-cors[]
 }
