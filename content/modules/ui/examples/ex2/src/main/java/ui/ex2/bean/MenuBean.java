2b2844609f945bead2b4ddb21085ff20b36a9014
Mariya Sukhova <sukhova@haulmont.com>
2022-08-16 12:59:05 +0400

    Document ability to pass parameters from menu item XML to bean and runnable class methods #388

--- content/modules/ui/examples/ex2/src/main/java/ui/ex2/bean/MenuBean.java
+++ content/modules/ui/examples/ex2/src/main/java/ui/ex2/bean/MenuBean.java
@@ -1,16 +1,22 @@
 package ui.ex2.bean;
 
+import io.jmix.core.common.util.ParamsMap;
 import io.jmix.ui.AppUI;
 import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.WebBrowserTools;
 import io.jmix.ui.screen.Screen;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ui.ex2.screen.city.CityBrowse;
 
-// tag::menu-bean[]
+import java.util.Map;
+
+// tag::start[]
 @Component("sample_MenuBean")
 public class MenuBean {
-
+    // end::start[]
+    // tag::menu-bean[]
     @Autowired
     private ScreenBuilders screenBuilders;
 
@@ -22,5 +28,22 @@ public class MenuBean {
                 .build()
                 .show();
     }
+    // end::menu-bean[]
+    // tag::method-with-params[]
+
+    @Autowired
+    private ObjectProvider<WebBrowserTools> webBrowserToolsProvider;
+
+    public void openLink(Map<String, Object> parameters) {
+        String url = (String) parameters.get("url");
+        if (url == null) {
+            return;
+        }
+
+        webBrowserToolsProvider.getObject().showWebPage(
+                url, ParamsMap.of("target", "_blank"));
+    }
+    // end::method-with-params[]
+    // tag::end[]
 }
-// end::menu-bean[]
+// end::end[]
