670f6d9e4603280d9c61a0a26a23ffed011b18a8
Mariya Sukhova <sukhova@haulmont.com>
2022-08-23 16:53:35 +0400

    Describe the layout element of screen descriptor #420
    (cherry picked from commit d7c460652ae661a7cfdcc3b485142a46f5602dc1)

---
+++ content/modules/ui/examples/ex1/src/main/java/ui/ex1/screen/containers/layout/LayoutScreen2.java
@@ -0,0 +1,10 @@
+package ui.ex1.screen.containers.layout;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("sample_LayoutScreen2")
+@UiDescriptor("layout-screen2.xml")
+public class LayoutScreen2 extends Screen {
+}
