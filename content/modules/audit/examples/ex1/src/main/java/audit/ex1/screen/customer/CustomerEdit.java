b7fb1920a27d97fa6d67044141a3df429c8e998e
sukhova <sukhova@haulmont.com>
2022-12-14 12:53:47 +0400

    Entity snapshots #442

--- content/modules/audit/examples/ex1/src/main/java/audit/ex1/screen/customer/CustomerEdit.java
+++ content/modules/audit/examples/ex1/src/main/java/audit/ex1/screen/customer/CustomerEdit.java
@@ -1,10 +1,39 @@
 package audit.ex1.screen.customer;
 
+import io.jmix.audit.snapshot.EntitySnapshotManager;
+import io.jmix.auditui.screen.snapshot.SnapshotDiffViewer;
+import io.jmix.core.EntityStates;
 import io.jmix.ui.screen.*;
 import audit.ex1.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("ex1_Customer.edit")
 @UiDescriptor("customer-edit.xml")
 @EditedEntityContainer("customerDc")
 public class CustomerEdit extends StandardEditor<Customer> {
+    // tag::load-fragment[]
+    @Autowired
+    protected EntityStates entityStates;
+
+    @Autowired
+    protected SnapshotDiffViewer snapshotDiff;
+
+    @Subscribe
+    protected void onBeforeShow(BeforeShowEvent event) {
+        if (!entityStates.isNew(getEditedEntity())) {
+            snapshotDiff.loadVersions(getEditedEntity());
+        }
+    }
+    // end::load-fragment[]
+
+    // tag::create-snapshot[]
+    @Autowired
+    protected EntitySnapshotManager entitySnapshotManager;
+
+    @Subscribe
+    protected void onAfterCommitChanges(AfterCommitChangesEvent event) {
+        entitySnapshotManager.createSnapshot(getEditedEntity(),
+                getEditedEntityContainer().getFetchPlan());
+    }
+    // end::create-snapshot[]
 }
