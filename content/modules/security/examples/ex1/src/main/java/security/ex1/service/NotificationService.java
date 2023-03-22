a2d4b067ac23840456e78d7b0a50ada0704585cb
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-03-22 17:24:37 +0400

    Provide samples how to check that user has access to entity or entity attribute #393

--- content/modules/security/examples/ex1/src/main/java/security/ex1/service/NotificationService.java
+++ content/modules/security/examples/ex1/src/main/java/security/ex1/service/NotificationService.java
@@ -1,18 +1,42 @@
 package security.ex1.service;
 
 import io.jmix.core.AccessManager;
+import io.jmix.core.Metadata;
+import io.jmix.core.accesscontext.CrudEntityContext;
 import io.jmix.core.accesscontext.SpecificOperationAccessContext;
+import io.jmix.core.metamodel.model.MetaClass;
+import io.jmix.ui.WindowConfig;
+import io.jmix.ui.WindowInfo;
+import io.jmix.ui.accesscontext.UiShowScreenContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import security.ex1.entity.Customer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class NotificationService {
 
-    // tag::specific-permission[]
+    // tag::access-manager[]
     @Autowired
     private AccessManager accessManager;
 
+    // end::access-manager[]
+
+    // tag::metadata[]
+    @Autowired
+    private Metadata metadata;
+
+    // end::metadata[]
+
+    // tag::window-config[]
+    @Autowired
+    private WindowConfig windowConfig;
+
+    // end::window-config[]
+
+    // tag::specific-permission[]
     public void notifyCustomer(Customer customer) {
         SpecificOperationAccessContext accessContext =
                 new SpecificOperationAccessContext("customer.notify");
@@ -22,4 +46,27 @@ public class NotificationService {
         }
     }
     // end::specific-permission[]
+
+    // tag::crud-permission[]
+    public boolean checkCustomerReadPermitted() {
+        MetaClass metaClass = metadata.getClass(Customer.class);
+        CrudEntityContext accessContext = new CrudEntityContext(metaClass);
+        accessManager.applyRegisteredConstraints(accessContext);
+        return accessContext.isReadPermitted();
+    }
+    // end::crud-permission[]
+
+    // tag::all-permitted-screens[]
+    public List<String> getPermittedScreens() {
+        return windowConfig.getWindows().stream()
+                .map(WindowInfo::getId)
+                .filter(screenId -> {
+                    UiShowScreenContext accessContext = new UiShowScreenContext(screenId);
+                    accessManager.applyRegisteredConstraints(accessContext);
+                    return accessContext.isPermitted();
+                })
+                .collect(Collectors.toList());
+    }
+    // end::all-permitted-screens[]
+
 }
