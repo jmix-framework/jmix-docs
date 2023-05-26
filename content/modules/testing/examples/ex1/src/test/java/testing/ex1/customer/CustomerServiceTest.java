1e4836e38b630b000856399038738c7a5407a193
Mario David <mario@die-davids.de>
2023-05-26 10:13:51 +0200

    add first part of integration tests

--- content/modules/testing/examples/ex1/src/test/java/testing/ex1/customer/CustomerServiceTest.java
+++ content/modules/testing/examples/ex1/src/test/java/testing/ex1/customer/CustomerServiceTest.java
@@ -1,23 +1,16 @@
 package testing.ex1.customer;
 
 import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import testing.ex1.app.customer.CustomerService;
 import testing.ex1.entity.Customer;
-import testing.ex1.entity.Order;
-import testing.ex1.entity.OrderLine;
-import testing.ex1.entity.Product;
 
 import javax.sql.DataSource;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,10 +29,10 @@ public class CustomerServiceTest {
     @Autowired // <1>
     DataSource dataSource;
 
-    @AfterEach // <2>
+    @AfterEach// <2>
     void tearDown() {
         JdbcTemplate jdbc = new JdbcTemplate(dataSource);
-        jdbc.execute("DELETE FROM CUSTOMER"); // <3>
+        JdbcTestUtils.deleteFromTables(jdbc, "CUSTOMER"); // <3>
     }
     // end::cleanup-customer-table[]
 
@@ -69,9 +62,6 @@ public class CustomerServiceTest {
         customer.setEmail("customer@test.com");
         dataManager.save(customer);
 
-        assertThat(dataManager.load(Customer.class).all().list())
-                .hasSize(1);
-
         // when
         Optional<Customer> foundCustomer = customerService.findByEmail("other-customer@company.com");
 
@@ -79,8 +69,4 @@ public class CustomerServiceTest {
         assertThat(foundCustomer)
                 .isNotPresent();
     }
-
-    // ...
-    // end::order-line-event-listener-test[]
-
 }
