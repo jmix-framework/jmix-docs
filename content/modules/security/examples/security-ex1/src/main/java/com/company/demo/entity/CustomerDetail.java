1968e9c51bae1f50085dcab50339758a5546ea66
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-06-12 20:40:42 +0400

    Refactor Security for FlowUI

---
+++ content/modules/security/examples/security-ex1/src/main/java/com/company/demo/entity/CustomerDetail.java
@@ -0,0 +1,72 @@
+package com.company.demo.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "CUSTOMER_DETAIL")
+@Entity(name = "CustomerDetail")
+public class CustomerDetail {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @Column(name = "CONTENT")
+    @Lob
+    private String content;
+
+    @Column(name = "CONFIDENTIAL")
+    private Boolean confidential;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CUSTOMER_ID")
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Boolean getConfidential() {
+        return confidential;
+    }
+
+    public void setConfidential(Boolean confidential) {
+        this.confidential = confidential;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
