6e5900d262241c4759e34673de0ba62c0e71c7b6
Sergei Iudaev <s.iudaev@haulmont.com>
2023-06-16 19:46:59 +0400

    Document Flow UI tabs #501
    (cherry picked from commit 443f09a70b474676ec21dcf955013d9e51502fb8)

---
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/tabs/TabsView.java
@@ -0,0 +1,48 @@
+package com.company.onboarding.view.component.tabs;
+
+
+import com.company.onboarding.view.main.MainView;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "TabsView", layout = MainView.class)
+@ViewController("TabsView")
+@ViewDescriptor("tabs-view.xml")
+public class TabsView extends StandardView {
+
+    // tag::content[]
+    @ViewComponent
+    private VerticalLayout content;
+
+    // end::content[]
+
+    @Autowired
+    private UiComponents uiComponents;
+
+    // tag::SelectedChangeEvent[]
+    @Subscribe("tabs")
+    public void onTabsSelectedChange(final Tabs.SelectedChangeEvent event) {
+        setTabContent(event.getSelectedTab());
+    }
+    // end::SelectedChangeEvent[]
+
+    // tag::SetTabContent[]
+    private void setTabContent(Tab tab) {
+        content.removeAll();
+        Label tabLabel = uiComponents.create(Label.class);
+        if ("tab1".equals(tab.getId().orElse(null))) {
+            tabLabel.setText("Tab One is selected");
+            content.add(tabLabel);
+        } else {
+            tabLabel.setText("Tab Two is selected");
+            content.add(tabLabel);
+        }
+    }
+    // end::SetTabContent[]
+}
