e2985b1230f45f4f6a6829fe07f27805ac072af7
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-06-24 20:51:31 +0400

    Update Files and UI Properties for 2.0

---
+++ content/modules/files/examples/files-ex1/src/main/java/com/company/demo/DemoApplication.java
@@ -0,0 +1,66 @@
+package com.company.demo;
+
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import io.jmix.core.FileStorage;
+import io.jmix.localfs.LocalFileStorage;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Push
+@Theme(value = "files-ex1")
+@PWA(name = "Files Ex1", shortName = "Files Ex1")
+@SpringBootApplication
+public class DemoApplication implements AppShellConfigurator {
+
+    @Autowired
+    private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource")
+    DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource.hikari")
+    DataSource dataSource(final DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @EventListener
+    public void printApplicationUrl(final ApplicationStartedEvent event) {
+        LoggerFactory.getLogger(DemoApplication.class).info("Application started at "
+                + "http://localhost:"
+                + environment.getProperty("local.server.port")
+                + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
+    }
+
+    // tag::multiple-fs[]
+    @Bean
+    FileStorage myFileStorage() {
+        return new LocalFileStorage("myfs", "/var/tmp/myfs");
+    }
+    // end::multiple-fs[]
+
+}
