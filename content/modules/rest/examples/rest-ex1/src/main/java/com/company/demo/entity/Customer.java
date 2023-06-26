b6cc44007476a3d56f5cf2e669db25f1e12a12bb
Maxim Gorbunkov <m.gorbunkov@haulmont.com>
2023-06-26 19:13:16 +0400

    REST API and Authorization Server documentation

---
+++ content/modules/rest/examples/rest-ex1/src/main/java/com/company/demo/entity/Customer.java
@@ -0,0 +1,55 @@
+package com.company.demo.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "CUSTOMER")
+@Entity
+public class Customer {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "FIRST_NAME", nullable = false)
+    @NotNull
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    @NotNull
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
