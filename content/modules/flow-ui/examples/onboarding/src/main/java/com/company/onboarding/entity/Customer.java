ea380c50ce60e5b379e73a3c92258f8f80c0e2a3
Sergei Iudaev <s.iudaev@haulmont.com>
2023-06-28 20:18:29 +0400

    Document Flow UI genericFilter #490
    (cherry picked from commit 34cc02d6a65fae246061140b3780da6b083e7a1f)

---
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/entity/Customer.java
@@ -0,0 +1,129 @@
+package com.company.onboarding.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "CUSTOMER", indexes = {
+        @Index(name = "IDX_CUSTOMER_CITY", columnList = "CITY_ID")
+})
+@Entity
+public class Customer {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @JoinColumn(name = "CITY_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private City city;
+
+    @Column(name = "LEVEL_")
+    private String level;
+
+    @Column(name = "AGE")
+    private Integer age;
+
+    @Column(name = "MARTIAL_STATUS")
+    private String martialStatus;
+
+    @Column(name = "HOBBY")
+    private String hobby;
+
+    @InstanceName
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "REWARD_POINTS")
+    private String rewardPoints;
+
+    public String getRewardPoints() {
+        return rewardPoints;
+    }
+
+    public void setRewardPoints(String rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getMartialStatus() {
+        return martialStatus;
+    }
+
+    public void setMartialStatus(String martialStatus) {
+        this.martialStatus = martialStatus;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
