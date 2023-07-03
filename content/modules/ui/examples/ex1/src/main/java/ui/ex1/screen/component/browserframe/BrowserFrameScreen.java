ee7632f3bc63ab98c724f2a725d77e7105285f86
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-07-03 11:03:24 +0400

    Remove classic UI

--- content/modules/ui/examples/ex1/src/main/java/ui/ex1/screen/component/browserframe/BrowserFrameScreen.java
+++
@@ -1,8 +0,0 @@
-package ui.ex1.screen.component.browserframe;
-
-import io.jmix.ui.screen.*;
-
-@UiController("sample_BrowserFrameScreen")
-@UiDescriptor("browser-frame-screen.xml")
-public class BrowserFrameScreen extends Screen {
-}
