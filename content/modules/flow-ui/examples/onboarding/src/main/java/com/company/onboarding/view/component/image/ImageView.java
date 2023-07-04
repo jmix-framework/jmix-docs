3b19ee12c356c97880ac58dfe76dcc2fec20ddec
Sergei Iudaev <s.iudaev@haulmont.com>
2023-07-04 12:07:38 +0400

    Document Flow UI image HTML element #516
    (cherry picked from commit 8415539766c7e96249f7613318a28496a22bec92)

---
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/image/ImageView.java
@@ -0,0 +1,12 @@
+package com.company.onboarding.view.component.image;
+
+import com.company.onboarding.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "ImageView", layout = MainView.class)
+@ViewController("ImageView")
+@ViewDescriptor("image-view.xml")
+public class ImageView extends StandardView {
+
+}
---
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/imageresources/ImageResourcesView.java
@@ -0,0 +1,45 @@
+package com.company.onboarding.view.component.imageresources;
+
+
+import com.company.onboarding.view.main.MainView;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import io.jmix.flowui.component.image.JmixImage;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.view.*;
+
+@Route(value = "ImageResourcesView", layout = MainView.class)
+@ViewController("ImageResourcesView")
+@ViewDescriptor("image-resources-view.xml")
+public class ImageResourcesView extends StandardView {
+
+    @ViewComponent
+    private JmixImage image;
+
+
+    @Subscribe("staticBtn")
+    public void onStaticBtnClick(final ClickEvent<JmixButton> event) {
+        //tag::image-static[]
+        image.setSrc("/icons/icon.png");
+        //end::image-static[]
+    }
+
+    @Subscribe("urlBtn")
+    public void onUrlBtnClick(final ClickEvent<JmixButton> event) {
+        //tag::image-url[]
+        image.setSrc("https://www.jmix.io/uploads/framework_image_9efadbc372.svg");
+        //end::image-url[]
+    }
+
+    @Subscribe("streamBtn")
+    public void onStreamBtnClick(final ClickEvent<JmixButton> event) {
+        //tag::image-stream[]
+        StreamResource streamResource = new StreamResource("icon",
+                ()-> getClass().getResourceAsStream("/META-INF/resources/icons/icon.png"));
+        image.setSrc(streamResource);
+        //end::image-stream[]
+    }
+
+}
