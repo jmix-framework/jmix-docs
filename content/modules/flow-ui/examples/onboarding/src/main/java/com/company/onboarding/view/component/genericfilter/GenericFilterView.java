226fcf86093ae4aed52bbd608b8dbe7c6c104d22
Sergei Iudaev <s.iudaev@haulmont.net>
2023-08-18 18:51:02 +0400

    Document Flow UI genericFilter #490
    (cherry picked from commit 1bf666c6487fd2758cf202eca005c7cf688b9ae0)

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/genericfilter/GenericFilterView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/genericfilter/GenericFilterView.java
@@ -4,13 +4,19 @@ package com.company.onboarding.view.component.genericfilter;
 import com.company.onboarding.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.core.metamodel.model.MetaPropertyPath;
+import io.jmix.flowui.view.*;
 
 @Route(value = "GenericFilterView", layout = MainView.class)
 @ViewController("GenericFilterView")
 @ViewDescriptor("generic-filter-view.xml")
 public class GenericFilterView extends StandardView {
+
+    // tag::propertyFiltersPredicate[]
+    @Install(to = "genericFilter", subject = "propertyFiltersPredicate")
+    private boolean genericFilterPropertyFiltersPredicate(final MetaPropertyPath metaPropertyPath) {
+        return !metaPropertyPath.getMetaProperty().getName().equals("hobby");
+    }
+    // end::propertyFiltersPredicate[]
 
 }
