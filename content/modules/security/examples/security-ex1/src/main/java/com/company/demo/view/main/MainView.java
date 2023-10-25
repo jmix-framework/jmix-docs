62723841668a268f6fae5e49a4673a2287a63b0a
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-10-25 19:25:22 +0400

    What's new in 2.1

--- content/modules/security/examples/security-ex1/src/main/java/com/company/demo/view/main/MainView.java
+++ content/modules/security/examples/security-ex1/src/main/java/com/company/demo/view/main/MainView.java
@@ -1,12 +1,25 @@
 package com.company.demo.view.main;
 
+import com.company.demo.entity.User;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.usersubstitution.CurrentUserSubstitution;
 import io.jmix.flowui.app.main.StandardMainView;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("")
 @ViewController("MainView")
 @ViewDescriptor("main-view.xml")
 public class MainView extends StandardMainView {
+
+    // tag::user-substitution[]
+    @Autowired
+    private CurrentUserSubstitution currentUserSubstitution;
+
+    private String getSubstitutedUserName() {
+        User substitutedUser = (User) currentUserSubstitution.getSubstitutedUser();
+        return substitutedUser == null ? "" : substitutedUser.getUsername();
+    }
+    // end::user-substitution[]
 }
