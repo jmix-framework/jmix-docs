163c33c22e28cf47f074c81a2a5576da24ebe164
sukhova <sukhova@haulmont.com>
2024-02-14 14:50:21 +0400

    Update the Grid Export Actions add-on section #615

---
+++ content/modules/grid-export/examples/gridexport-ex1/src/main/java/com/company/demo/exporters/CustomExporter.java
@@ -0,0 +1,43 @@
+package com.company.demo.exporters;
+
+import com.vaadin.flow.component.grid.Grid;
+import io.jmix.flowui.download.Downloader;
+import io.jmix.gridexportflowui.exporter.DataGridExporter;
+import io.jmix.gridexportflowui.exporter.ExportMode;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+// tag::CustomExporter[]
+@Component("ui_CustomExporter")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CustomExporter implements DataGridExporter {
+    @Override
+    public void exportDataGrid(Downloader downloader, Grid<Object> dataGrid, ExportMode exportMode) {
+        //your custom logic
+    }
+
+    @Override
+    public String getLabel() {
+        return "custom";
+    }
+
+    @Override
+    public void addColumnValueProvider(String columnId, Function<ColumnValueContext, Object> columnValueProvider) {
+        //your custom logic
+    }
+
+    @Override
+    public void removeColumnValueProvider(String columnId) {
+        //your custom logic
+    }
+
+    @Override
+    public Function<ColumnValueContext, Object> getColumnValueProvider(String columnId) {
+        //your custom logic
+        return null;
+    }
+}
+// end::CustomExporter[]
