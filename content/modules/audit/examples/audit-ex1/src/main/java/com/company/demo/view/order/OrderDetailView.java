bbdad655ff8edf0bc0d5bbf156da31c66dae963b
sukhova <sukhova@haulmont.com>
2024-02-22 14:35:11 +0400

    Update the Audit add-on section #610

--- content/modules/audit/examples/audit-ex1/src/main/java/com/company/demo/view/order/OrderDetailView.java
+++ content/modules/audit/examples/audit-ex1/src/main/java/com/company/demo/view/order/OrderDetailView.java
@@ -6,15 +6,24 @@ import com.company.demo.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
 import io.jmix.audit.entity.EntityLogItem;
+import io.jmix.audit.snapshot.EntitySnapshotManager;
 import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "orders/:id", layout = MainView.class)
 @ViewController("Order_.detail")
 @ViewDescriptor("order-detail-view.xml")
 @EditedEntityContainer("orderDc")
 public class OrderDetailView extends StandardDetailView<Order> {
+    // tag::snapshot[]
+    @Autowired
+    private EntitySnapshotManager entitySnapshotManager;
+
+    // end::snapshot[]
+
     // tag::order-java[]
     @ViewComponent
     private CollectionLoader<EntityLogItem> entityLogItemsDl;
@@ -25,4 +34,11 @@ public class OrderDetailView extends StandardDetailView<Order> {
         entityLogItemsDl.load();
     }
     // end::order-java[]
+    // tag::snapshot[]
+    @Subscribe(target = Target.DATA_CONTEXT)
+    public void onPostSave(final DataContext.PostSaveEvent event) {
+        entitySnapshotManager.createSnapshot(getEditedEntity(),
+                getEditedEntityContainer().getFetchPlan());
+    }
+    // end::snapshot[]
 }
