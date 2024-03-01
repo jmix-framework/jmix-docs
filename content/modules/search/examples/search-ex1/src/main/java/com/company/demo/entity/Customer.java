da90e5614b9941c5ea8203e8ad9c7ae0d5b7e19a
sukhova <sukhova@haulmont.com>
2024-03-01 16:35:19 +0400

    Update Search add-on section #651
    (cherry picked from commit 3e46bae4b7d2876905c207ad5ab0d987b6a27105)

---
+++ content/modules/search/examples/search-ex1/src/main/java/com/company/demo/entity/Customer.java
@@ -0,0 +1,75 @@
+package com.company.demo.entity;
+
+import io.jmix.core.FileRef;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "CUSTOMER")
+@Entity
+public class Customer {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "STATUS")
+    private String status;
+
+    @Column(name = "CARD", length = 1024)
+    private FileRef card;
+
+    public FileRef getCard() {
+        return card;
+    }
+
+    public void setCard(FileRef card) {
+        this.card = card;
+    }
+
+    public Status getStatus() {
+        return status == null ? null : Status.fromId(status);
+    }
+
+    public void setStatus(Status status) {
+        this.status = status == null ? null : status.getId();
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
