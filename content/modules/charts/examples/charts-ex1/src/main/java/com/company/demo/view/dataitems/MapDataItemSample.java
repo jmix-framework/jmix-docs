6afbb107d3f1882d3ff14e7bfbc885a222328322
Amir Chelly <amirchelly99@gmail.com>
2024-03-11 15:05:23 +0300

    completed first draft biding

---
+++ content/modules/charts/examples/charts-ex1/src/main/java/com/company/demo/view/dataitems/MapDataItemSample.java
@@ -0,0 +1,40 @@
+package com.company.demo.view.dataitems;
+
+import io.jmix.chartsflowui.component.Chart;
+import io.jmix.chartsflowui.data.item.MapDataItem;
+import io.jmix.chartsflowui.kit.component.model.DataSet;
+import io.jmix.chartsflowui.kit.data.chart.ListChartItems;
+import io.jmix.flowui.view.*;
+
+import java.util.List;
+import java.util.Map;
+
+@ViewController("map-data-item")
+@ViewDescriptor("map-data-item.xml")
+public class MapDataItemSample extends StandardView {
+
+    // tag::onInit[]
+    @ViewComponent
+    protected Chart chart;
+
+    @Subscribe
+    protected void onInit(InitEvent event) {
+        ListChartItems<MapDataItem> chartItems = new ListChartItems<>(
+                List.of(
+                        new MapDataItem(Map.of("value", 75, "description", "Sky")),
+                        new MapDataItem(Map.of("value", 7, "description", "Shady side of pyramid")),
+                        new MapDataItem(Map.of("value", 18, "description", "Sunny side of pyramid"))
+                )
+        );
+
+        chart.withDataSet(
+                new DataSet().withSource(
+                        new DataSet.Source<MapDataItem>()
+                                .withDataProvider(chartItems)
+                                .withCategoryField("description")
+                                .withValueField("value")
+                )
+        );
+    }
+    // end::onInit[]
+}
