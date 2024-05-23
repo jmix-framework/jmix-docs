e3cea2a5a734aa716e1305be4030e3c2676850c9
Sergei Iudaev <s.iudaev@haulmont.com>
2024-05-23 13:35:37 +0400

    Improve TextArea doc #695

---
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/entity/Order.java
@@ -0,0 +1,39 @@
+package com.company.onboarding.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "ORDER__1")
+@Entity(name = "Order_")
+public class Order {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "DESCRIPTION")
+    @Lob
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
