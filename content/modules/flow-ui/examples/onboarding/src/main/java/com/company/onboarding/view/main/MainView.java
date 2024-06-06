d8d68c711afd6cddfeea8d690fa96f31195053c8
sukhova <sukhova@haulmont.com>
2024-06-06 17:03:37 +0400

    Document Initial Layout in Main View #764
    (cherry picked from commit 85512cc56aaf25019ed7edb608afa98150fa30ca)

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/main/MainView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/main/MainView.java
@@ -1,7 +1,12 @@
 package com.company.onboarding.view.main;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.app.main.StandardMainView;
+import io.jmix.flowui.component.image.JmixImage;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.ViewComponent;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 
@@ -9,4 +14,20 @@ import io.jmix.flowui.view.ViewDescriptor;
 @ViewController("MainView")
 @ViewDescriptor("main-view.xml")
 public class MainView extends StandardMainView {
+    // tag::initialLayout[]
+    @ViewComponent
+    private JmixImage<Object> urlImage;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        urlImage.setSrc("https://www.jmix.io/uploads/framework_image_9efadbc372.svg");
+        urlImage.setWidth("100%");
+        urlImage.setHeight("100%");
+    }
+
+    @Subscribe(id = "urlImage", subject = "singleClickListener")
+    public void onUrlImageClick(final ClickEvent<JmixImage<?>> event) {
+        Notification.show("Clicked!");
+    }
+    // end::initialLayout[]
 }
