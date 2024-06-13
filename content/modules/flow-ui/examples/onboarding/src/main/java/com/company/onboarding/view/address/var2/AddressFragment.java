910ed380ed4a1a287ec1c5251da93386c7b82f9f
Gleb Gorelov <glebfox@gmail.com>
2024-06-13 23:51:53 +0400

    Document Fragments (776)

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/address/var2/AddressFragment.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/address/var2/AddressFragment.java
@@ -7,18 +7,22 @@ import io.jmix.flowui.component.textfield.TypedTextField;
 import io.jmix.flowui.fragment.Fragment;
 import io.jmix.flowui.fragment.FragmentDescriptor;
 import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.view.ViewComponent;
 
 // tag::fragment-java[]
 @FragmentDescriptor("address-fragment.xml")
 public class AddressFragment extends Fragment<FormLayout> {
 
+    @ViewComponent
+    private EntityComboBox<City> cityField;
+    @ViewComponent
+    private TypedTextField<String> zipcodeField;
+
     public void setCitiesContainer(CollectionContainer<City> citiesContainer) { // <1>
-        EntityComboBox<City> cityField = getInnerComponent("cityField");
-        cityField.setItems(citiesContainer); // <2>
+        cityField.setItems(citiesContainer);
     }
 
     public void setZipcodePlaceholder(String placeholder) {
-        TypedTextField<String> zipcodeField = getInnerComponent("zipcodeField");
         zipcodeField.setPlaceholder(placeholder);
     }
 }
