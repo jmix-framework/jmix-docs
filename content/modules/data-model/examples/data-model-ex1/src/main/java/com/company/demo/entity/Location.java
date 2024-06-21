9e0bb3dc7075edaa2eed74e55ab421c7ce9b1723
sukhova <sukhova@haulmont.com>
2024-06-21 16:38:05 +0400

    Document Commenting Data Model #746

--- content/modules/data-model/examples/data-model-ex1/src/main/java/com/company/demo/entity/Location.java
+++ content/modules/data-model/examples/data-model-ex1/src/main/java/com/company/demo/entity/Location.java
@@ -2,29 +2,35 @@ package com.company.demo.entity;
 
 import com.company.demo.entity.validation.ValidZipCode;
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.Comment;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+// tag::comment[]
+@Comment("""
+        Stores information about locations.
+        Has reference to address""")
 @JmixEntity
 @Table(name = "LOCATION")
 @Entity
 public class Location {
-    @JmixGeneratedValue
-    @Column(name = "ID", nullable = false)
-    @Id
-    private UUID id;
 
+    @Comment("Location zip code")
     @InstanceName
     @Column(name = "ZIP_CODE", nullable = false, length = 10)
     @ValidZipCode
     private String zipCode;
-
+    // end::comment[]
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
     @Column(name = "ADDRESS")
     private String address;
 
