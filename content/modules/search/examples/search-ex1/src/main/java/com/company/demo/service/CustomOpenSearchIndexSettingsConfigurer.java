8c9f193876357b09a70f0612ed13e244105fc254
Ivan Gavrilov <i.gavrilov@haulmont.com>
2024-06-21 18:27:04 +0400

    Update search docs: OpenSearch support and backlog 790

---
+++ content/modules/search/examples/search-ex1/src/main/java/com/company/demo/service/CustomOpenSearchIndexSettingsConfigurer.java
@@ -0,0 +1,47 @@
+package com.company.demo.service;
+
+import com.company.demo.entity.Order;
+import io.jmix.searchopensearch.index.OpenSearchIndexSettingsConfigurationContext;
+import io.jmix.searchopensearch.index.OpenSearchIndexSettingsConfigurer;
+import org.opensearch.client.opensearch.indices.IndexSettings;
+import org.springframework.stereotype.Component;
+
+// tag::index-settings-configurer[]
+@Component
+public class CustomOpenSearchIndexSettingsConfigurer implements OpenSearchIndexSettingsConfigurer {
+
+    // end::index-settings-configurer[]
+    // tag::configure[]
+    @Override
+    public void configure(OpenSearchIndexSettingsConfigurationContext context) {
+        // end::configure[]
+        // tag::configure-body[]
+        IndexSettings.Builder commonSettingsBuilder = context.getCommonSettingsBuilder();
+        commonSettingsBuilder
+                .maxResultWindow(15000)
+                .analysis(analysisBuilder ->
+                        analysisBuilder.analyzer("customized_standard", analyzerBuilder ->
+                                analyzerBuilder.standard(stdAnalyzerBuilder ->
+                                        stdAnalyzerBuilder.maxTokenLength(100)
+                                )
+                        )
+                );
+
+        IndexSettings.Builder orderSettingsBuilder = context.getEntitySettingsBuilder(Order.class);
+        orderSettingsBuilder
+                .maxResultWindow(15000)
+                .maxRegexLength(2000)
+                .analysis(analysisBuilder ->
+                        analysisBuilder.analyzer("customized_standard", analyzerBuilder ->
+                                analyzerBuilder.standard(stdAnalyzerBuilder ->
+                                        stdAnalyzerBuilder.maxTokenLength(150)
+                                )
+                        )
+                );
+        // end::configure-body[]
+        // tag::configure[]
+    }
+    // end::configure[]
+    // tag::index-settings-configurer[]
+}
+// end::index-settings-configurer[]
