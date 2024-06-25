ac58ddecffcb41ee0e177e88f35f61595fd3b028
sukhova <sukhova@haulmont.com>
2024-06-25 14:30:48 +0400

    Document the gridColumnVisibility component #794

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/step/StepListView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/step/StepListView.java
@@ -3,7 +3,10 @@ package com.company.onboarding.view.step;
 import com.company.onboarding.entity.Step;
 import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.grid.DataGridColumnVisibilityChangedEvent;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "steps", layout = MainView.class)
 @ViewController("Step.list")
@@ -11,4 +14,12 @@ import io.jmix.flowui.view.*;
 @LookupComponent("stepsTable")
 @DialogMode(width = "50em", height = "37.5em")
 public class StepListView extends StandardListView<Step> {
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe("stepsTable.name")
+    public void onStepsTableNameDataGridColumnVisibilityChanged(final DataGridColumnVisibilityChangedEvent<Step> event) {
+        notifications
+                .show("Visible: " + event.isVisible());
+    }
 }
