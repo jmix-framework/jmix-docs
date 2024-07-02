fd413aabdd26ce85ab0e37f1dfce496aaf2b8fd1
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-07-02 13:33:42 +0400

    What's new in 1.6

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/security/HrManagerRowLevelRole.java
+++
@@ -1,7 +0,0 @@
-package com.company.onboarding.security;
-
-import io.jmix.security.role.annotation.RowLevelRole;
-
-@RowLevelRole(name = "HrManagerRowLevelRole", code = "hr-manager-row-level-role")
-public interface HrManagerRowLevelRole {
-}
