00af2c858f0b90ec2b1fab6fab51e1349265776f
sukhova <sukhova@haulmont.com>
2024-07-16 16:35:26 +0400

    Improve Upload doc #700
    (cherry picked from commit c83d9a58830d4b828128a5976aea80a4b3ca2dce)

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/upload/UploadView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/upload/UploadView.java
@@ -3,22 +3,25 @@ package com.company.onboarding.view.component.upload;
 
 import com.company.onboarding.view.main.MainView;
 
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.upload.*;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.upload.JmixUpload;
 import io.jmix.flowui.view.*;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Route(value = "UploadView", layout = MainView.class)
 @ViewController("UploadView")
 @ViewDescriptor("upload-view.xml")
 public class UploadView extends StandardView {
-    // tag::upload[]
-    @ViewComponent
-    private JmixUpload upload;
 
-    // end::upload[]
     // tag::notifications[]
     @Autowired
     private Notifications notifications;
@@ -32,4 +35,14 @@ public class UploadView extends StandardView {
                 .show();
     }
     // end::FileRejectedEvent[]
+    // tag::SucceededEvent[]
+    @Subscribe("upload")
+    public void onUploadSucceeded(final SucceededEvent event) {
+        notifications.create(
+                "Your file %s has been uploaded successfully.".formatted(
+                        event.getFileName()))
+                .withThemeVariant(NotificationVariant.LUMO_PRIMARY)
+                .show();
+    }
+    // end::SucceededEvent[]
 }
