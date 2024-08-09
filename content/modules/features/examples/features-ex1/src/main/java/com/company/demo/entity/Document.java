a23234202c0f6000fe5bd007b40ffdc036dc0bfb
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-08-09 18:41:22 +0400

    Update Other Features project

---
+++ content/modules/features/examples/features-ex1/src/main/java/com/company/demo/entity/Document.java
@@ -0,0 +1,48 @@
+package com.company.demo.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "DOCUMENT")
+@Entity
+public class Document {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @Column(name = "NUMBER_")
+    private Long number;
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
---
+++ content/modules/features/examples/features-ex1/src/main/java/com/company/demo/view/document/DocumentListView.java
@@ -0,0 +1,15 @@
+package com.company.demo.view.document;
+
+import com.company.demo.entity.Document;
+import com.company.demo.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+
+@Route(value = "documents", layout = MainView.class)
+@ViewController("Document.list")
+@ViewDescriptor("document-list-view.xml")
+@LookupComponent("documentsDataGrid")
+@DialogMode(width = "64em")
+public class DocumentListView extends StandardListView<Document> {
+}
