0a3770262ee35b90909a9d592e4bbe955b4077ab
Bryan Yin <hanbing.yin@icloud.com>
2024-08-16 13:18:07 +0800

    sync latest 2.3 changes

---
+++ content/modules/features/examples/features-ex1/src/main/java/com/company/demo/view/document/DocumentDetailView.java
@@ -0,0 +1,43 @@
+package com.company.demo.view.document;
+
+import com.company.demo.entity.Document;
+import com.company.demo.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.data.Sequence;
+import io.jmix.data.Sequences;
+import io.jmix.flowui.model.DataContext;
+import io.jmix.flowui.model.InstanceContainer;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route(value = "documents/:id", layout = MainView.class)
+@ViewController("Document.detail")
+@ViewDescriptor("document-detail-view.xml")
+@EditedEntityContainer("documentDc")
+public class DocumentDetailView extends StandardDetailView<Document> {
+
+    //tag::sequences-inject[]
+    @Autowired
+    private Sequences sequences;
+    //end::sequences-inject[]
+
+    //tag::sequences-use[]
+    @ViewComponent
+    private InstanceContainer<Document> documentDc;
+
+    @Subscribe(target = Target.DATA_CONTEXT)
+    public void onPreSave(final DataContext.PreSaveEvent event) {
+        Long number = sequences.createNextValue(Sequence.withName("document_number") // <1>
+                .setStore("additional") // <2>
+                .setStartValue(10) // <3>
+                .setIncrement(10)); // <4>
+        documentDc.getItem().setNumber(number);
+    }
+    //end::sequences-use[]
+
+    private void generateUniqueNumber() {
+        //tag::sequences-simple[]
+        Long nextNumber = sequences.createNextValue(Sequence.withName("some_seq"));
+        //end::sequences-simple[]
+    }
+}
--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/checkboxgroup/CheckboxgroupView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/checkboxgroup/CheckboxgroupView.java
@@ -2,19 +2,27 @@ package com.company.onboarding.view.component.checkboxgroup;
 
 
 import com.company.onboarding.entity.Hobby;
+import com.company.onboarding.entity.OnboardingStatus;
+import com.company.onboarding.entity.User;
 import com.company.onboarding.view.main.MainView;
-
 import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import io.jmix.core.FileRef;
+import io.jmix.core.FileStorage;
 import io.jmix.flowui.Notifications;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.component.checkboxgroup.JmixCheckboxGroup;
 import io.jmix.flowui.kit.component.ComponentUtils;
 import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -36,7 +44,18 @@ public class CheckboxgroupView extends StandardView {
     private JmixCheckboxGroup<Integer> ratingCheckboxGroup;
 
     // end::ratingCheckboxGroup[]
+    // tag::checkboxGroupEnum[]
+    @ViewComponent
+    private JmixCheckboxGroup<OnboardingStatus> checkboxGroupEnum;
 
+    // end::checkboxGroupEnum[]
+    // tag::renderer[]
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private FileStorage fileStorage;
+
+    // end::renderer[]
     @Subscribe("getBtn")
     public void onGetBtnClick(final ClickEvent<JmixButton> event) {
         notifications.create("Values: " + checkboxGroup.getTypedValue()).show();
@@ -47,17 +66,44 @@ public class CheckboxgroupView extends StandardView {
     public void onInit(final InitEvent event) {
         // end::onInit[]
         // tag::setItems[]
-        checkboxGroupInt.setItems(new ArrayList<>(Arrays.asList(1,2,3,4,5)));
+        checkboxGroupInt.setItems(1, 2, 3, 4, 5);
         // end::setItems[]
         // tag::setItemsMap[]
-        Map<Integer,String> map = new LinkedHashMap<>();
-        map.put(2,"Poor");
-        map.put(3,"Average");
-        map.put(4,"Good");
-        map.put(5,"Excellent");
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(2, "Poor");
+        map.put(3, "Average");
+        map.put(4, "Good");
+        map.put(5, "Excellent");
         ComponentUtils.setItemsMap(ratingCheckboxGroup, map);
         // end::setItemsMap[]
+        // tag::setItemsEnum[]
+        checkboxGroupEnum.setItems(OnboardingStatus.class);
+        // end::setItemsEnum[]
         // tag::onInit[]
     }
+
     // end::onInit[]
+    // tag::renderer[]
+    @Supply(to = "userCheckboxGroup", subject = "renderer")
+    private ComponentRenderer<HorizontalLayout, User> userCheckboxGroupRenderer() {
+        return new ComponentRenderer<>(user -> {
+            FileRef fileRef = user.getPicture();
+            HorizontalLayout row = uiComponents.create(HorizontalLayout.class);
+            row.setAlignItems(FlexComponent.Alignment.END);
+            if (fileRef != null) {
+                Image image = uiComponents.create(Image.class);
+                image.setWidth("30px");
+                image.setHeight("30px");
+                image.setClassName("user-picture");
+                StreamResource streamResource = new StreamResource(
+                        fileRef.getFileName(),
+                        () -> fileStorage.openStream(fileRef));
+                image.setSrc(streamResource);
+                row.add(image);
+            }
+            row.add(new Span(user.getFirstName() + ", " + user.getLastName()));
+            return row;
+        });
+    }
+    // end::renderer[]
 }
