e923db4d65c74c49d91dd872d53f9bad1d66ad3e
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-08-21 12:57:23 +0400

    Kanban documentation [WiP]

--- content/modules/kanban/examples/kanban-ex1/src/main/java/com/company/onboarding/view/kanban/KanbanView.java
+++ content/modules/kanban/examples/kanban-ex1/src/main/java/com/company/onboarding/view/kanban/KanbanView.java
@@ -4,10 +4,13 @@ package com.company.onboarding.view.kanban;
 import com.company.onboarding.entity.KanbanTask;
 import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
 import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.view.*;
 import io.jmix.kanbanflowui.component.Kanban;
 import io.jmix.kanbanflowui.component.data.ContainerKanbanItems;
+import io.jmix.kanbanflowui.kit.component.JmixKanban;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.UUID;
 
@@ -21,9 +24,16 @@ public class KanbanView extends StandardView {
     private Kanban<KanbanTask> programmaticallyKanban;
 
     // end::kanban[]
+    // tag::programmaticPropertiesMappingKanban[]
+    @ViewComponent
+    private Kanban<KanbanTask> programmaticPropertiesMappingKanban;
+
+    // end::programmaticPropertiesMappingKanban[]
     // tag::tasksDc[]
     @ViewComponent
     private CollectionContainer<KanbanTask> kanbanTasksDc;
+    @Autowired
+    private DataManager dataManager;
 
     // end::tasksDc[]
 
@@ -34,7 +44,20 @@ public class KanbanView extends StandardView {
         // tag::setItems[]
         programmaticallyKanban.setItems(new ContainerKanbanItems<>(kanbanTasksDc, UUID.class));
         // end::setItems[]
+        // tag::programmaticPropertiesMapping[]
+        JmixKanban.PropertiesMapping propertiesMapping = new JmixKanban.PropertiesMapping("id", "status", "text");
+        propertiesMapping.setUserAvatar("assignee.picture");
+        propertiesMapping.setUsername("assignee.username");
+        programmaticPropertiesMappingKanban.setPropertiesMapping(propertiesMapping);
+        // end::programmaticPropertiesMapping[]
         // tag::onInit[]
     }
     // end::onInit[]
+
+    // tag::kanbanSaveDelegate[]
+    @Install(to = "saveDelegateKanban", subject = "saveDelegate")
+    private void kanbanSaveDelegate(final KanbanTask kanbanTask) {
+        dataManager.save(kanbanTask);
+    }
+    // end::kanbanSaveDelegate[]
 }
