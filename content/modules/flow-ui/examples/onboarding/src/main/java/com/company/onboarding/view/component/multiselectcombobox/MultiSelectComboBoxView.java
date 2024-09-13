582f050a9533de8e7ffc95f2674f206b9fb6d674
Bryan Yin <hanbing.yin@icloud.com>
2024-09-13 17:06:34 +0800

    sync to 2.3.3

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/multiselectcombobox/MultiSelectComboBoxView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/component/multiselectcombobox/MultiSelectComboBoxView.java
@@ -1,21 +1,55 @@
 package com.company.onboarding.view.component.multiselectcombobox;
 
 
+import com.company.onboarding.entity.Hobby;
 import com.company.onboarding.view.main.MainView;
 
 import com.vaadin.flow.component.shared.HasClientValidation;
+import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.Subscribe;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+import java.util.stream.Stream;
 
 @Route(value = "MultiSelectComboBoxView", layout = MainView.class)
 @ViewController("MultiSelectComboBoxView")
 @ViewDescriptor("multi-select-combo-box-view.xml")
 public class MultiSelectComboBoxView extends StandardView {
-    @Subscribe("multiSelectComboBox")
-    public void onMultiSelectComboBoxClientValidated(final HasClientValidation.ClientValidatedEvent event) {
-        
+
+    // tag::load-hobbies[]
+    @Autowired
+    protected DataManager dataManager;
+
+    protected Collection<Hobby> hobbies;
+
+    // end::load-hobbies[]
+
+    // tag::onInit[]
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        // end::onInit[]
+        // tag::load-hobbies[]
+        hobbies = dataManager.load(Hobby.class).all().list();
+        // end::load-hobbies[]
+        // tag::onInit[]
     }
+
+    // end::onInit[]
+
+    // tag::itemsFetchCallback[]
+    @Install(to = "hobbiesComboBox", subject = "itemsFetchCallback")
+    private Stream<Hobby> hobbiesComboBoxItemsFetchCallback(final Query<Hobby, String> query) {
+        String enteredValue = query.getFilter()
+                .orElse("");
+
+        return hobbies.stream()
+                .filter(hobby -> hobby.getName() != null &&
+                        hobby.getName().toLowerCase().contains(enteredValue.toLowerCase()))
+                .skip(query.getOffset())
+                .limit(query.getLimit());
+    }
+    // end::itemsFetchCallback[]
 }
