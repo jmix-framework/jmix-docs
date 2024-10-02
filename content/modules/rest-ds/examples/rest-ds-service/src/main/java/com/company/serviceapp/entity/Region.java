07b9f0ec190c46fb19656939c3e911682ee5502e
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-10-02 19:23:58 +0400

    Describe REST DataStore #884

---
+++ content/modules/rest-ds/examples/rest-ds-service/src/main/java/com/company/serviceapp/entity/Region.java
@@ -0,0 +1,56 @@
+package com.company.serviceapp.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+// tag::jpa-entity[]
+@JmixEntity
+@Table(name = "REGION")
+@Entity
+public class Region {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+
+    // getters and setters
+    // end::jpa-entity[]
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
