c6c19692d5bd812b000d33bb294ff8c9f4603471
sukhova <sukhova@haulmont.com>
2024-10-25 15:11:08 +0400

    PivotTable add-on documentation #844

---
+++ content/modules/pivot-table/examples/pivottable-ex1/src/main/java/com/company/ex1/entity/Month.java
@@ -0,0 +1,40 @@
+package com.company.ex1.entity;
+
+import io.jmix.core.metamodel.datatype.EnumClass;
+import org.springframework.lang.Nullable;
+
+public enum Month implements EnumClass<String> {
+
+    JANUARY("january"),
+    FEBRUARY("february"),
+    MARCH("march"),
+    APRIL("april"),
+    MAY("may"),
+    JUNE("june"),
+    JULY("july"),
+    AUGUST("august"),
+    SEPTEMBER("september"),
+    OCTOBER("october"),
+    NOVEMBER("november"),
+    DECEMBER("december");
+
+    private final String id;
+
+    Month(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Nullable
+    public static Month fromId(String id) {
+        for (Month at : Month.values()) {
+            if (at.getId().equals(id)) {
+                return at;
+            }
+        }
+        return null;
+    }
+}
