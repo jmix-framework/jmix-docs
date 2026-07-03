6a5def6af6d7835bb7bbc4ecc8b80aaeeb33b1e7
sukhova <sukhova@haulmont.com>
2026-07-03 11:34:36 +0400

    Describe the Resources interface #104

--- content/modules/features/examples/features-ex1/src/main/java/com/company/demo/view/document/DocumentDetailView.java
+++ content/modules/features/examples/features-ex1/src/main/java/com/company/demo/view/document/DocumentDetailView.java
@@ -3,6 +3,7 @@ package com.company.demo.view.document;
 import com.company.demo.entity.Document;
 import com.company.demo.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.Resources;
 import io.jmix.data.Sequence;
 import io.jmix.data.Sequences;
 import io.jmix.flowui.model.DataContext;
@@ -10,12 +11,33 @@ import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @Route(value = "documents/:id", layout = MainView.class)
 @ViewController("Document.detail")
 @ViewDescriptor("document-detail-view.xml")
 @EditedEntityContainer("documentDc")
 public class DocumentDetailView extends StandardDetailView<Document> {
 
+    protected static final String SRC_PATH = "META-INF/resources/html/html-file.html";
+    //tag::resources-example[]
+    @Autowired
+    private Resources resources;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        try (InputStream stream = resources.getResourceAsStream(SRC_PATH)) {
+            if (stream == null) {
+                // resource not found
+                return;
+            }
+            // use the stream
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //end::resources-example[]
     //tag::sequences-inject[]
     @Autowired
     private Sequences sequences;
